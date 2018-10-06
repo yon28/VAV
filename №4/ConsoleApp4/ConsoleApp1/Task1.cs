f542f6a7e6e0386575514ae2fc1182958b9f536f
yon28 <yon_@mail.ru>
2018-10-06 14:19:22 +0300

    +4

---
+++ №4/ConsoleApp4/ConsoleApp1/Task1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи строку (например, предложение).");
+            string str = Console.ReadLine();
+            string[] A = str.Split(' ', '.', ';', ',', '(', ')', '[', ']', '<', '>', '+', '-', '}', '{', '/', '*', '=');
+            int S = 0;
+            foreach (var word in A)
+            {
+                S = S + word.Length;
+            }
+            Console.WriteLine("Средняя длина слов: " + S/A.Length +".");
+            Console.ReadLine();
+        }
+    }
+}
